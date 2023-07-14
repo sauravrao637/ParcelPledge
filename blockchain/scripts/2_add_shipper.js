f96389668775525557c30dcf1ca8f21e0f0cb2ff
camo0112 <56369484+camo0112@users.noreply.github.com>
2023-07-14 09:22:09 +0530

    updated scripts

--- blockchain/scripts/2_add_shipper.js
+++ blockchain/scripts/2_add_shipper.js
@@ -1,16 +1,17 @@
 const CamoParcel = artifacts.require("CamoParcel");
-const { addShipper, GANCHE, SHIPPER_ADDR } = require('./0_utils');
+const { addShipper, GANCHE, SHIPPERS_ADDR } = require('./0_utils');
 
 module.exports = async function (callback) {
 	try {
 		const accounts = await web3.eth.getAccounts();
 		const owner = accounts[0];
-		const shipperv = GANCHE ? accounts[1] : SHIPPER_ADDR;
+		const shippers = GANCHE ? [accounts[1]] : SHIPPERS_ADDR;
 
 		const camoParcel = await CamoParcel.deployed();
 		console.log("const addrParcel = " + "\"" + camoParcel.address + "\"");
-
-		await addShipper(camoParcel, owner, shipperv);
+		for (let i = 0; i < shippers.length; i++) {
+			await addShipper(camoParcel, owner, shippers[i]);
+		}
 	} catch (error) {
 		console.log(error);
 	}
